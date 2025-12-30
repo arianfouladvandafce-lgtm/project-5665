71c107a39b02ac2a3c02d5498ea0592e465d4ca4
Areo Mche <areo.mche.ce@gmail.com>
2025-12-30 12:34:34 +0330

    second

--- Bit-game.cpp
+++ Bit-game.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int sum = 0;
-    int playerMove, aiMove;
+    int User, AI;
+    int max, num1, num2;
+    cout << "please enter a number to starting the game: \n";
+    cin >> max;
+    cout << "pleaes enter two number for chossing each time: .\n";
+    cin >> num1 >> num2;
+    cout << "Whoever reaches"<< max << "wins\n\n";
 
-    cout << "Game of 20 (Player vs AI)\n";
-    cout << "You can choose 1 or 2 each turn.\n";
-    cout << "Whoever reaches 20 wins.\n\n";
-
-    while (true) {
+    while(true)
+    {
         // Player turn
         cout << "Current sum: " << sum << endl;
         cout << "Your move (1 or 2): ";
-        cin >> playerMove;
+        cin >> User;
 
-        if (playerMove != 1 && playerMove != 2) {
-            cout << "Invalid move! Choose 1 or 2.\n";
-            continue;
-        }
+        if User != 1 && User != 2)
+            {
+                cout << "Invalid move! Choose 1 or 2.\n";
+                continue;
+            }
 
-        sum += playerMove;
+        sum += User;
 
-        if (sum >= 20) {
+        if (sum >= 20)
+        {
             cout << "🎉 You win!\n";
             break;
         }
 
         // AI turn
         if (sum % 3 == 0)
-            aiMove = 1;
+            AI = 1;
         else
-            aiMove = 3 - (sum % 3);
+            AI = 3 - (sum % 3);
 
-        cout << "AI chooses: " << aiMove << endl;
-        sum += aiMove;
+        cout << "AI chooses: " << AI << endl;
+        sum += AI;
 
-        if (sum >= 20) {
+        if (sum >= 20)
+        {
             cout << "🤖 AI wins!\n";
             break;
         }
